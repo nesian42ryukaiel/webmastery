cbc3a75d46d0ccbfefede4634bc5bd4945aa4340
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-05-08 23:37:38 +0900

    classified dice functions by object

--- js/dice.js
+++ js/dice.js
@@ -1,14 +1,14 @@
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min);
-  //The maximum is exclusive and the minimum is inclusive
-}
-
-function bell() {
-  return getRandomInt(1, 6) + getRandomInt(1, 6) + getRandomInt(1, 6);
-}
-
-function check() {
-  return getRandomInt(1, 20);
-}
+let Dice = {
+  getRandomInt: function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+    //The maximum is exclusive and the minimum is inclusive
+  },
+  bell: function() {
+    return Dice.getRandomInt(1, 6) + Dice.getRandomInt(1, 6) + Dice.getRandomInt(1, 6);
+  },
+  check: function() {
+    return Dice.getRandomInt(1, 20);
+  }
+};
